8206cac17160efd90cb8b0f55334fd8bdc0bfcd5
razod <masonboulier@gmail.com>
2025-07-31 12:07:07 -0400

    remove unnecessary imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRef, useEffect, useState } from 'react'
+import { useRef, useEffect } from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import Loader from '@/components/Loader';
