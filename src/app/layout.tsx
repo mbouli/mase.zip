4c131e2b1ccd58f3bd49960e4e9d027256244fbb
razod <masonboulier@gmail.com>
2025-09-19 13:27:02 -0500

    fix meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "ROOM125",
-  description: "ROOM125 by Mason Boulier",
+  title: "MASE.ZIP",
+  description: "Mason Boulier's photography archive",
 };
 
 export default function RootLayout({
